0e79e095cabfc21737e0978faec9a44dacc59f9f
LatteCappuchino <Honorx8user@gmail.com>
2024-11-14 20:06:27 +0300

    Create functions2.lua

---
+++ functions2.lua
@@ -0,0 +1,23 @@
+Here, you will learn about functions.
+
+function maths(num1, num2)
+print(num1+num2)
+end
+maths(1, 1)
+
+This starts with a function that had to be used by firing it.
+  Function's name is "maths", it has 2 "variables", num1 and num2.
+    All it does is prints num1+num2.
+maths(1, 1)
+    This fires the function, and gives it 2 variables. 1 and 1.
+      It will output 2, because the function checks the given variables, and uses them in itself.
+        It does:
+        print(1+1)
+function Print()
+  print("Hello!")
+        end
+        Print()
+        This function doesn't need nor use variables.
+          All it does is print "Hello!" to the Console.
+          To fire it, we must do Print()
+            All4Now. Cya
